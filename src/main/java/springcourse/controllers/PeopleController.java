4475624601c4919cc4d5223b1ab56466ca73d404
Senchenko Volodymyr <lif4xiks@gmail.com>
2023-01-10 22:30:33 +0200

    Added Get method that redirect to html form to add new person,Post method that get object from html form and add it to array list

--- src/main/java/springcourse/controllers/PeopleController.java
+++ src/main/java/springcourse/controllers/PeopleController.java
@@ -1,13 +1,10 @@
 package springcourse.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.method.annotation.ModelFactory;
+import org.springframework.web.bind.annotation.*;
 import springcourse.dao.PersonDAO;
+import springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -29,5 +26,17 @@ public class PeopleController {
         //get one person by its id from DAO and redirect ot to representation
         model.addAttribute("person",personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person")Person person){
+        //model.addAttribute("person",new Person());
+        return ("people/new");
+    }
+
+    @PostMapping("")
+    public String create(@ModelAttribute("person")Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/springcourse/models/Person.java
+++ src/main/java/springcourse/models/Person.java
@@ -3,7 +3,7 @@ package springcourse.models;
 public class Person {
     private int id;
     private String name;
-
+    public Person() {}
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
